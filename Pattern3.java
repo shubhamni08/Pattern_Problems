a3047bae0ac2b071c2251ea09455edf220dc3405
shubhamni08 <nikamshubham016@gmail.com>
2024-09-23 17:08:48 +0530

    Pattern 3 code Added

---
+++ Pattern3.java
@@ -0,0 +1,33 @@
+package pattern_Problems;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		int n= 5;
+		//star1
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<i+1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		//star2
+		for(int i=0;i<n-1;i++) {
+			for(int j=0;j<n-i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//*
+//**
+//***
+//****
+//*****
+//****
+//***
+//**
+//*
+
