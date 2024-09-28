12d247d5ec90f40ecd22108e204e2f1e147efb64
shubhamni08 <nikamshubham016@gmail.com>
2024-09-28 21:22:48 +0530

    Pattern 10 Code Added

---
+++ Pattern10.java
@@ -0,0 +1,41 @@
+package pattern_Problems;
+
+public class Pattern10 {
+
+	public static void main(String[] args) {
+		int n = 7;
+		int n1 = n/2;
+		//first half
+		for(int i=0;i<=n1;i++) {
+			//spaces
+			for(int j=0;j<n1-i;j++) {
+				System.out.print(" ");
+			}
+			//stars
+			for(int j=0;j<i+1;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		//second half
+		for(int i=0;i<=n1;i++) {
+			//spaces
+			for(int j=0;j<i+1;j++) {
+				System.out.print(" ");
+			}
+			//stars
+			for(int j=0;j<n1-i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//   * 
+//  * * 
+// * * * 
+//* * * * 
+// * * * 
+//  * * 
+//   * 
---
+++ Pattern9.java
@@ -0,0 +1,45 @@
+package pattern_Problems;
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		int n1 = n/2;
+		System.out.println(n1);
+		int k=n1+1;
+//		System.out.println(k);
+		//first half
+		for(int i=0;i<=n1;i++) {
+			//spaces
+			for(int j=0;j<i;j++) {
+				System.out.print("_");
+			}
+			//stars
+			for(int j=0;j<k;j++) {
+				System.out.print("* ");
+			}
+			k--;
+			System.out.println();
+		}
+		//second half
+		for(int i=0;i<n1;i++) {
+			//spaces
+			for(int j=0;j<n1-i;j++) {
+				System.out.print("_");
+			}
+			//stars
+			for(int j=0;j<i+1;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//   * 
+//  * * 
+// * * * 
+//* * * * 
+// * * * 
+//  * * 
+//   * 
