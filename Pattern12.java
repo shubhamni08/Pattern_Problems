ecc8efb20ab88fd0d564555f00522edafdbf12ba
shubhamni08 <nikamshubham016@gmail.com>
2024-09-29 12:44:06 +0530

    Pattern 12 Code Added

---
+++ Pattern12.java
@@ -0,0 +1,21 @@
+package pattern_Problems;
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i=0;i<n;i++) {
+			int k=1;
+			for(int j=0;j<i+1;j++) {	
+				System.out.print(k+++" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
+//1 
+//1 2 
+//1 2 3 
+//1 2 3 4 
+//1 2 3 4 5 
