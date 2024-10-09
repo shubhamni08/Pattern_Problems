5c41da88fc5d07b7a4c5065bd1990d15e879eecc
shubhamni08 <nikamshubham016@gmail.com>
2024-10-09 11:36:47 +0530

    Pattern 12 code updated

--- Pattern12.java
+++ Pattern12.java
@@ -4,13 +4,20 @@ public class Pattern12 {
 
 	public static void main(String[] args) {
 		int n = 5;
-		for(int i=0;i<n;i++) {
-			int k=1;
-			for(int j=0;j<i+1;j++) {	
-				System.out.print(k+++" ");
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {	
+				System.out.print(j+" ");
 			}
 			System.out.println();
 		}
+		
+		for(int i=n;i>1;i--) {
+			for(int j=1;j<i;j++) {	
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
@@ -19,3 +26,7 @@ public class Pattern12 {
 //1 2 3 
 //1 2 3 4 
 //1 2 3 4 5 
+//1 2 3 4 
+//1 2 3
+//1 2 
+//1 
