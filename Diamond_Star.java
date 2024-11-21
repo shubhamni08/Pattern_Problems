d333736e6bcb8258e7bb0a9e7d5bf908e440cb87
Shubham Nikam <nikamshubham016@gmail.com>
2024-11-21 18:50:29 +0530

    Update Diamond_Star.java

--- Diamond_Star.java
+++ Diamond_Star.java
@@ -7,6 +7,7 @@ public class Diamond_Star {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
 		int n = sc.nextInt();
 		//number of rows
 		for(int i=0;i<n;i++) {
