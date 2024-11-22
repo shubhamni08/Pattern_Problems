e65d5f185f86c29bcdaf916a4e7c3488f6077331
Shubham Nikam <nikamshubham016@gmail.com>
2024-11-22 22:07:04 +0530

    Update Pattern11.java

--- Pattern11.java
+++ Pattern11.java
@@ -1,16 +1,13 @@
 package pattern_Problems;
 
 public class Pattern11 {
-	
 //			i	star 
 //*****		0	 5    	condition= i=0 OR i=4 for rows print whole row star 
 //*   *  	1 	 1   			   j=0 OR j=4 for column print stars
 //*   *		2	 1
 //*   *		3	 1
 //*****		4	 5
-	
 	public static void main(String[] args) {
-		
 		int n = 5;
 		for(int i=0;i<n;i++) {
 			for(int j=0;j<n;j++) {	
