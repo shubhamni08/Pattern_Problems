84a2d393fa404d2c863bfdd1d2e741a1764591b7
Shubham Nikam <nikamshubham016@gmail.com>
2024-11-26 14:16:04 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -3,7 +3,6 @@ package pattern_Problems;
 import java.util.Scanner;
 
 public class Pattern1 {
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
