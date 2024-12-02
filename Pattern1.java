0d7ba72d90cbf67f9f944f89d238df578e00b755
Shubham Nikam <nikamshubham016@gmail.com>
2024-12-02 10:28:07 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Pattern1 {
 	
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for(int i=0;i<n;i++) {
